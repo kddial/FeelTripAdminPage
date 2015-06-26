b73d3e1b0e5d9c55b336aa975f061e49b13104b4
Kevin Dial <kevin.dial.phone@gmail.com>
2015-06-25 21:50:45 -0400

    fix correct types for db

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// set page version to tell clients correct version
+var pageVersion = 2;
 
 // initialize Parse api
 var api = JSON.parse(config);
@@ -8,7 +10,13 @@ $(document).ready(function() {
   // prevent inputs from saving values
   $("form :input").attr("autocomplete", "off");
 
-  // fill in sample event
+  // set page version
+  $("#page-version").html("Version " + pageVersion.toString());
+
+  // save test objects
+  // testParseObject();
+
+  // fill in sample event function
   sampleEvent();
 
   // animate loading button
@@ -22,18 +30,35 @@ $(document).ready(function() {
     startSubmit();
 
     // get the event data
+    // string input
     var data = {};
     data.name = $("#nameInput").val();
     data.description = $("#descriptionInput").val();
     data.address = $("#addressInput").val();
-    data.price = $("#priceInput").val();
-    data.date_start = $("#dateStartInput").val();
-    data.date_end = $("#dateEndInput").val();
-    data.mood = $("#moodInput").val();
-    data.hashtags = $("#hashTagsInput").val();
-    data.website = $("#websiteInput").val();
-    data.phone_number = $("#phoneInput").val();
     data.email = $("#emailInput").val();
+    data.comments = $("#commentsInput").val();
+    data.website = $("#websiteInput").val();
+    
+    // number input
+    data.price = JSON.parse($("#priceInput").val());
+    data.phone_number = JSON.parse($("#phoneInput").val());
+
+    // array input
+    data.mood = $("#moodInput").val().split(",").map(Number).filter(Boolean);
+    data.hashtags = $("#hashTagsInput").val().split(",").map(function(s) {return s.trim()}).filter(Boolean);
+
+    // date input
+    //    pass in array as arugments
+    //    source: http://stackoverflow.com/questions/11291206/passing-an-array-to-the-javascript-date-constructor-is-it-standard
+    var dateArray = parseDate($("#dateStartInput").val());
+    data.date_start = new (Function.prototype.bind.apply(
+                            Date, [null].concat(dateArray)));
+
+    dateArray = parseDate($("#dateEndInput").val());
+
+    data.date_end = new (Function.prototype.bind.apply(
+                            Date, [null].concat(dateArray)));
+    
 
     // get the event picture
     var imageUpload = $("#imageInput")[0];
@@ -45,7 +70,8 @@ $(document).ready(function() {
     }
 
     // Save the event to Parse
-    var event = Parse.Object.extend("Events");
+    // var event = Parse.Object.extend("Events");
+    var event = Parse.Object.extend("EventsTest3"); // testing purposes
     var eventObject = new event();
     eventObject.save(data, {
       success:function() {
@@ -70,8 +96,6 @@ $(document).ready(function() {
         alert("error!");
       }
     });
-
-
   });
 });
 
@@ -93,25 +117,25 @@ function endSubmit() {
   $("#loading-button").hide();
 }
 
-
-/* Fill in sample event info */
+/* Fill in sample event info. Uses FILL IN SAMPLE button on UI. */
 function sampleEvent() {
   $("#sample-event").click(function() {
-    $("#nameInput").prop("value", "test");
-    $("#descriptionInput").prop("value", "test");
-    $("#addressInput").prop("value", "test event");
+    $("#nameInput").prop("value", "test Beach party");
+    $("#descriptionInput").prop("value", "Let's have some fun in the sun! Bring your friends, family, dogs, beers, food, smiles!");
+    $("#addressInput").prop("value", "909 Lake Shore Boulevard West, Toronto, ON M6K 3L3");
     $("#priceInput").prop("value", 22);
-    $("#dateStartInput").prop("value", "[2015, 4, 22, 13, 0, 0]");
-    $("#dateEndInput").prop("value", "[2015, 4, 22, 13, 0, 0]");
-    $("#moodInput").prop("value", "[2]");
-    $("#hashTagsInput").prop("value", "[#test]");
+    $("#dateStartInput").prop("value", "2015, 4, 22, 13, 30");
+    $("#dateEndInput").prop("value", "2015, 4, 22, 14");
+    $("#moodInput").prop("value", "1, 2, 3");
+    $("#hashTagsInput").prop("value", "#test, #test2,#test3");
+    $("#commentsInput").prop("value", "This event occurs every sunday");
     $("#websiteInput").prop("value", "www.kevindial.com");
-    $("#phoneInput").prop("value", "4161234567");
+    $("#phoneInput").prop("value", 4161234567);
     $("#emailInput").prop("value", "kk@kk.com");
   });
 }
 
-/* Fade loading in and out */
+/* Animate loading button (fade in and out) */
 function animateLoadingButton() {
     var items = ["Loading", "Loading.", "Loading..", "Loading...", "Loading....", "Loading....."],
         $text = $( '#load-text' ),
@@ -133,4 +157,47 @@ function animateLoadingButton() {
         });
     }
     loop( delay );
+}
+
+/* Simple test to save Parse objects */
+function testParseObject() {
+  // Save the event to Parse
+  // var event = Parse.Object.extend("Events");
+  var event = Parse.Object.extend("EventsTest3"); // testing purposes
+  var eventObject = new event();
+
+  var data = {};
+
+  var currentDate = new Date();
+  data.date1 = currentDate;
+
+  // data.date2 = new Date([2015, 4, 22, 13]);
+  //                 yyyy, mm-1, dd, hh, mm, ss
+  data.date3 = new Date(2015, 3, 22, 1, 10, 20); // april 22, 1 am (EDT), 10 minutes, 20 seconds
+
+
+  eventObject.save(data, {
+    success:function() {
+      console.log("succes!");
+      alert("saved!");
+    },
+    error:function(error) {
+      console.log("error!");
+      console.dir(error);
+      alert("error!");
+    }
+  });
+}
+
+/* Parse Input Dates into an array.
+     Argument is date as a string.
+     Return date represented as an array of ints [yyyy, mm, dd, hh, mm, ss]
+*/
+function parseDate(inputDate) {
+
+    var dateArray = inputDate.split(",").map(Number).filter(Boolean);
+
+    // subtract 1 from month (0 == January, 1 == Feburary)
+    dateArray[1] = dateArray[1] - 1; 
+    return dateArray;
 }
